515be6b9240e9fbe4a3d49fff931ff8a693a53d3
Michael Durando <mjdurando82@yahoo.com>
2023-03-26 15:07:52 -0400

    added services folder for auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
 import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <h2>Hello tailwind</h2>
-    </div>
-  )
+  return <div className="App"></div>
 }
 
 export default App
