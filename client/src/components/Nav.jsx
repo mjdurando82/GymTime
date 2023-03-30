3e75f7a9797e21c848976a178311fd31db2bd6ed
Michael Durando <mjdurando82@yahoo.com>
2023-03-30 14:59:24 -0400

    navbar color changes

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -7,19 +7,19 @@ const Nav = ({ user, handleLogout }) => {
     {user && (
       <nav className='bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-slate-700 fixed w-full top-0 z-10'>
       <header className='container flex flex-wrap items-center justify-evenly'>
-      <Link to='/home' className='hover:text-blue-700 text-gray-800 font-medium py-2 sm:py-1 px-2 sm:px-4 rounded-lg transition-all duration-300'>
-      Home
+      <Link to='/home' className='hover:text-blue-700 text-slate-50 font-medium py-2 sm:py-1 px-2 sm:px-4 rounded-lg transition-all duration-300'>
+        Home
       </Link>
-      <Link to='/workout' className='hover:text-blue-700 text-gray-800 font-medium py-2 sm:py-1 px-2 sm:px-4 rounded-lg transition-all duration-300'>
+      <Link to='/workout' className='hover:text-blue-700 text-slate-50 font-medium py-2 sm:py-1 px-2 sm:px-4 rounded-lg transition-all duration-300'>
       Workout
       </Link>
-      <Link to='/history' className='hover:text-blue-700 text-gray-800 font-medium py-2 sm:py-1 px-2 sm:px-4 rounded-lg transition-all duration-300'>
+      <Link to='/history' className='hover:text-blue-700 text-slate-50 font-medium py-2 sm:py-1 px-2 sm:px-4 rounded-lg transition-all duration-300'>
       History
       </Link>
-      <Link to='/feed' className='hover:text-blue-700 text-gray-800 font-medium py-2 sm:py-1 px-2 sm:px-4 rounded-lg transition-all duration-300'>
+      <Link to='/feed' className='hover:text-blue-700 text-slate-50 font-medium py-2 sm:py-1 px-2 sm:px-4 rounded-lg transition-all duration-300'>
       Feed
       </Link>
-      <Link onClick={handleLogout} to='/' className='hover:text-blue-700 text-gray-800 font-medium py-2 sm:py-1 px-2 sm:px-4 rounded-lg transition-all duration-300'>
+      <Link onClick={handleLogout} to='/' className='hover:text-blue-700 text-slate-50 font-medium py-2 sm:py-1 px-2 sm:px-4 rounded-lg transition-all duration-300'>
       Sign Out
       </Link>
       </header>
