d00a99594db29d431b6a320f1738f4e576e33f8c
Michael Durando <mjdurando82@yahoo.com>
2023-03-31 15:23:33 -0400

    comments on front end include username

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -63,9 +63,9 @@ const Feed = ({ user }) => {
             {post?.comments?.map((comment) => (
               <div key={comment._id} className="flex items-start mb-4">
                 <div>
-                  <p className="font-medium mb-1">{comment.user}</p>
+                  <p className="font-medium mb-1">{comment.user.username}</p>
                   <p className="text-gray-700">{comment.content}</p>
-                  {user?.id === comment?.user && (
+                  {user?.id === comment?.user._id && (
                     <div className="mt-2">
                       <button className="bg-red-500 text-white py-1 px-1 rounded-md text-sm mr-2 hover:bg-rose-700 transition-all duration-300" onClick={(e) => deleteComment(e, comment._id)}>Delete</button>
                       <button className="bg-slate-700 text-white py-1 px-1 rounded-md font-small text-sm md:text-sm hover:bg-blue-700 transition-all duration-300"onClick={(e) => openForm(e,comment._id)}>Edit</button>
