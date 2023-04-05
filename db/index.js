6ba9a1680128c1a3bfafb8b74e927a36298450ea
Michael Durando <mjdurando82@yahoo.com>
2023-04-05 10:22:37 -0400

    fix to deploy on heroku

--- db/index.js
+++ db/index.js
@@ -1,17 +1,15 @@
 const mongoose = require('mongoose')
 require('dotenv').config() // Add this line
 
-let MONGODB_URI = 'mongodb://127.0.0.1:27017/gymDatabase'
-
 mongoose
-  .connect(MONGODB_URI)
+  .connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log('Successfully connected to MongoDB.')
+    console.log('Successfully connected to MongoDB!')
   })
   .catch((e) => {
     console.error('Connection error', e.message)
   })
-
+mongoose.set('debug', true)
 const db = mongoose.connection
 
 module.exports = db
--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
 app.use(express.static(`${__dirname}/client/build`))
 
 // app.use() middleware here ^ ///////////////////
@@ -34,4 +35,5 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 app.get('/*', (req, res) => {
   res.sendFile(`${__dirname}/client/build/index.html`)
 })
+
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
