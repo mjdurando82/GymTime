9f5f38172aaeda1e83739e3f9ee140a028b60ad1
Michael Durando <mjdurando82@yahoo.com>
2023-04-05 16:11:42 -0400

    deployment fix

--- server.js
+++ server.js
@@ -37,5 +37,4 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 app.get('/*', (req, res) => {
   res.sendFile(`${__dirname}/client/build/index.html`)
 })
-
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
