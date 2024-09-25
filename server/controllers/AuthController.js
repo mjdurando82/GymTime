b1ab3872904fbe14b3cfc10384b2ed5cfe08dff6
Michael Durando <mjdurando82@yahoo.com>
2024-09-25 15:53:09 -0400

    middleware path fix

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,5 +1,5 @@
 const User = require('../models/user')
-const middleware = require('..middleware/')
+const middleware = require('../middleware/index')
 
 const Register = async (req, res) => {
   try {
